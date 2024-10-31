1f182b2424ade7603d2f937d35a0bd11fb373cdc
Stephen Mwangi <mwangiwahome70@gmail.com>
2024-10-31 09:18:21 +0300

    Added  PORT

--- SERVER/server.js
+++ SERVER/server.js
@@ -1,42 +1,43 @@
 // Import express framework
-import express from 'express'
+import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
 // Import book route
 // import router from './Routes/booksRoutes.js';
-import booksRouter from './Routes/booksRoutes.js'
+import booksRouter from './Routes/booksRoutes.js';
 
 // Import Book Model
-import { Book } from './Models/BookModel.js';
+import {Book} from './Models/BookModel.js';
 
 // initialize app
-const app = express();
-dotenv.config();
+const app = express ();
+dotenv.config ();
 
 // To allow JSON content
-app.use(express.json());
-app.use(cors());
+app.use (express.json ());
+app.use (cors ());
 // Path and callback function parameters
-app.get("/", (req,res)=>{
-    res.status(200).send("Welcome to MERN Book Store");
+app.get ('/', (req, res) => {
+  res.status (200).send ('Welcome to MERN Book Store');
+});
 
-})
-
-app.use("/books", booksRouter);
+app.use ('/books', booksRouter);
 // To connect to mongoose
-mongoose.connect(process.env.MONGO_URL).then(() => {
-    console.log("Successful connection to database");
+mongoose
+  .connect (process.env.MONGO_URL)
+  .then (() => {
+    console.log ('Successful connection to database');
 
     // The app should only listen to PORT when connection is successful
 
     // Listen to PORT
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server running on http://localhost:${PORT}`);
-})
-
-
-}).catch((error) => {
-    console.log("Connection to database Failed" + error);
-})
+    const PORT = process.env.PORT || 5555;
+    app.listen (PORT, () => {
+      console.log (`Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch (error => {
+    console.log ('Connection to database Failed' + error);
+  });
